38ae6769fcb99bbbe3cc085b4a5ccc61dce62fbd
Jehova Jehova <avohxj@gmail.com>
2025-04-06 06:55:25 +0700

    Refactor ready.js: simplify startup and enforce channel requirements

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,6 @@
 import { guildId, voiceChannelId, embedChannelId } from '../../config/config.js';
-import { logStartup } from '../utils/logger.js';
 import { initializeBot } from '../core/initializeBot.js';
-import { stations } from '../core/stations.js';
-import { handleSelectMenu } from '../embed/radioInterface.js';
-
-let currentStation = '1';
-let startTime = null;
-let embedMessage;
+import { logStartup } from '../utils/logger.js';
 
 export default async (client) => {
   const guild = guildId
@@ -23,42 +17,17 @@ export default async (client) => {
 
   if (!vc) {
     logStartup(`Failed to find voice channel in server: ${guild.name}`);
-    process.exit(1);
+    process.exit(2);
   }
 
   if (!tc) {
     logStartup(`Failed to find embed/text channel in server: ${guild.name}`);
-    process.exit(1);
+    process.exit(3);
   }
 
   logStartup(`Logged in as ${client.user.tag}`);
   logStartup(`Embed/Text channel: ${tc.name} (${tc.id})`);
   logStartup(`Voice channel: ${vc.name} (${vc.id})`);
 
-  const init = await initializeBot({
-    client,
-    stations,
-    setEmbedMessage: msg => embedMessage = msg,
-    getCurrentStation: () => currentStation,
-    setStartTime: val => (startTime = val),
-    voiceChannel: vc,
-    textChannel: tc
-  });
-
-  if (!init) process.exit(1);
-
-  const { play } = init;
-
-  client.on('interactionCreate', async interaction => {
-    await handleSelectMenu({
-      interaction,
-      stations,
-      currentStation,
-      play,
-      client,
-      embedMessage,
-      setStation: val => currentStation = val,
-      startTime
-    });
-  });
+  await initializeBot(client);
 };
